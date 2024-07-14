60d78cb90c0c1bc50af65b695f206b890fed47b3
Mohamed Jaddour <mjaddour1997@gmail.com>
2024-07-13 20:57:01 -0500

    module 2 completed baseline

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const ctrMain = require('../controllers/main');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
